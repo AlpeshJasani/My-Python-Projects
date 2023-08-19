ed3de53e283ec5fa9f713ce4dddf7e39a4904dcc
AlpeshJasani <112234589+AlpeshJasani@users.noreply.github.com>
2023-08-19 23:35:02 +0530

    Update rock-paper-scissors.py

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,23 +1,27 @@
 import random
 
 def play():
-    user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors\n")
-    computer = random.choice(['r', 'p', 's'])
+    com_score = user_score = 0
+    
+    while com_score != 5 and user_score != 5:
+        user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors : ")
+        computer = random.choice(['r', 'p', 's'])
 
-    if user == computer:
-        return 'It\'s a tie'
+        if user == computer:
+            print("It's a tie")
 
-    # r > s, s > p, p > r
-    if is_win(user, computer):
-        return 'You won!'
+        # r > s, s > p, p > r
+        elif (user == 'r' and computer == 's') or (user == 's' and computer == 'p') \
+            or (user == 'p' and computer == 'r'):
+            user_score += 1
+            print(f'You won!\tScore: Your: {user_score}\t Computer: {com_score}')
+        else:
+            com_score += 1
+            print(f'You lost!\tScore: Your: {user_score}\t Computer: {com_score}')
 
-    return 'You lost!'
+    if user_score == 5:
+        print("\nYaaHoo!! You won the Game!! :)\n")
+    else:
+        print("\nOpps!! You lose the Game!! :(\n")
 
-def is_win(player, opponent):
-    # return true if player wins
-    # r > s, s > p, p > r
-    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') \
-        or (player == 'p' and opponent == 'r'):
-        return True
-
-print(play())
+play()
